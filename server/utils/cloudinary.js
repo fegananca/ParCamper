1cca4c2e2c86957b093e98c30ed8ae5b50226d4d
Ignacio Coll García <nachcoll@gmail.com>
2022-06-16 17:12:21 +0200

    deleted unused feature.

--- server/utils/cloudinary.js
+++
@@ -1,8 +0,0 @@
-const cloudinary = require('cloudinary').v2;
-cloudinary.config({
-  cloud_name: 'dcsyzmig8',
-  api_key: '991251845169275',
-  api_secret: 'sdxwVAF-zkmaucS8Q0oIEu4Dgic',
-});
-
-module.exports = { cloudinary };
