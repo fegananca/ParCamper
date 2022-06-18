d2e9da3e70990d2af55d32942a9e44721ff43749
Ignacio Coll García <nachcoll@gmail.com>
2022-06-18 09:19:21 +0200

    fixing irene's mistakes :/

--- server/models/db.js
+++ server/models/db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const key = process.env.MONGO_KEY;
 
 mongoose.connect(
-  'mongodb+srv://irene:ardilla@cluster0.gv8zyci.mongodb.net/parcamper',
+  key,
   (err) => {
     if (!err) console.log('mongoDB connection succeeded');
     else console.log('Error in DB connection');
