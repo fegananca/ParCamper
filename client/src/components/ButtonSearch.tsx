50b9835f5bf217f92a2ec36c20bf3011fbd51ee6
irene <irenecagnoni91@gmail.com>
2022-06-21 15:19:31 +0200

    refactor unit test frontend

--- client/src/components/ButtonSearch.tsx
+++ client/src/components/ButtonSearch.tsx
@@ -1,19 +1,26 @@
-import { Link } from 'react-router-dom';
 import { Icon } from '@iconify/react';
+import './ButtonSearch.css';
 
-const ButtonSearch = () => {
+const ButtonSearch = ({ callBack }: { callBack?: Function }) => {
   return (
-      <Link to={'/search'}>
-    <button type="button" className="button-enter">
-        <Icon
-          className="enter-icon"
-          icon="akar-icons:map"
-          width={20}
-          height={20}
-        />
-      <p id="search-map">Map</p>
+    <button
+      onClick={() => {
+        if (callBack) {
+          return callBack();
+        }
+        return console.log('button click');
+      }}
+      type='button'
+      className='button-enter'
+    >
+      <Icon
+        className='enter-icon'
+        icon='akar-icons:map'
+        width={20}
+        height={20}
+      />
+      <p id='search-map'>Map</p>
     </button>
-      </Link>
   );
 };
 
--- client/src/components/MainPage.tsx
+++ client/src/components/MainPage.tsx
@@ -1,15 +1,22 @@
-import ButtonSearch from "./ButtonSearch";
+import ButtonSearch from './ButtonSearch';
+import { useNavigate } from 'react-router-dom';
 const mainVideo = require('../Pages/images/pexels-pavel-danilyuk-9121392.mp4');
 
 const MainPage = () => {
+  const navigate = useNavigate();
+
   return (
-    <div className="wrapper">
-      <video autoPlay muted loop id="myVideo">
-        <source src={mainVideo} type="video/mp4" />
+    <div className='wrapper'>
+      <video autoPlay muted loop id='myVideo'>
+        <source src={mainVideo} type='video/mp4' />
       </video>
-      <ButtonSearch></ButtonSearch>
-      <div className="logo-header">
-        <h1 id="name">PARCAMPER</h1>
+      <ButtonSearch
+        callBack={() => {
+          navigate('/search');
+        }}
+      ></ButtonSearch>
+      <div className='logo-header'>
+        <h1 id='name'>PARCAMPER</h1>
       </div>
     </div>
   );
